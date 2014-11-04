f4f5504fdc25537ffdad745ae046a04378cd2011
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-05 00:44:57 +1100

    js files

---
+++ js/fcs_orig.js
@@ -0,0 +1,21 @@
+var hostAddress = '847055f0c7ed496490d0d5a13a805d55.cloudapp.net';
+$(document).ready(function () {
+	var varUrl; 
+	function CallService() {
+		$.ajax({
+			url : varUrl, // Location of the service
+			method: 'GET',
+			success : function(msg) {//On Successful service call
+						alert('Email should be sent');
+					  }
+					  //,
+			//error: ServiceFailed // When Service call fails
+		});
+	}
+	
+	$("#btnEmailLicenceKey").click(function () {
+		debugger;
+		varUrl = "http://"+ hostAddress + "/Service.svc/EmailLicence/?name=" + $('#name').val() + "&companyName=" + $('#compname').val() + "&emailTo=" + $('#email').val() + "&productCode=SauceryNUnit";
+		CallService();
+	});
+});
