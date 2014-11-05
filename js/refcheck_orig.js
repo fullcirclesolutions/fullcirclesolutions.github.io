5e9cc4bda762c8604a257f43cf521f0ad886c612
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-05 14:41:12 +1100

    another go

--- js/refcheck_orig.js
+++ js/refcheck_orig.js
@@ -4,3 +4,10 @@ if (ref.match(/^https?:\/\/([^\/]+\.)?paypal\.com(\/|$)/i)) {
 } else {
 	alert("Not from paypal: " + ref);
 }
+
+if(ref == null){
+alert("ref is null");
+}
+if(ref == ''){
+alert("ref is empty");
+}
