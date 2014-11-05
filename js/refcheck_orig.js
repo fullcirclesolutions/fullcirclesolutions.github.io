4c4f9129a2e0c1ed901673c65d9d90abc0447679
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-05 15:10:06 +1100

    gateway

--- js/refcheck_orig.js
+++ js/refcheck_orig.js
@@ -3,6 +3,7 @@ if (ref.match(/^https?:\/\/([^\/]+\.)?paypal\.com(\/|$)/i)) {
 	alert("Came from paypal: " + ref);
 } else {
 	alert("Not from paypal: " + ref);
+	window.location.href = "http://fullcirclesolutions.com.au"
 }
 
 if(ref == null){
