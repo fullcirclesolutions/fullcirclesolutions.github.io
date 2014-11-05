d346be877bd35e496cef4d61e6f12a36c8be550e
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-05 15:30:20 +1100

    remove alert

--- js/refcheck_orig.js
+++ js/refcheck_orig.js
@@ -1,5 +1,4 @@
 var ref = document.referrer.toLowerCase();
 if (ref.indexOf("checkout-gateway.html") == -1) {
-	alert("didnt come through the right door: ");
 	window.location.href = "http://fullcirclesolutions.com.au"
 }
