2de2cc20e631041e30f4ea828616b796376811e0
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-08 08:53:39 +1100

    print stuff out

--- js/fcs_orig.js
+++ js/fcs_orig.js
@@ -1,9 +1,15 @@
 function do_it(event) {
 	alert("here");
-	alert(event.data.name);
-	alert(event.data.company);
-	alert(event.data.email);
-	alert(event.data.product);
+	//alert(event.data.name);
+	//alert(event.data.company);
+	//alert(event.data.email);
+	//alert(event.data.product);
+	nameVal = $('#name').val();
+	companyVal = $('#company').val();
+	emailVal = $('#email').val();
+	alert("nameVal INSIDE=" + nameVal);
+	alert("companyVal INSIDE=" + companyVal);
+	alert("emailVal INSIDE=" + emailVal);
 	var hostAddress = '847055f0c7ed496490d0d5a13a805d55.cloudapp.net';
 	var url = "http://"+ hostAddress + "/Service.svc/EmailLicence/?name=" + event.data.name + "&companyName=" + event.data.company + "&emailTo=" + event.data.email + "&productCode=" + event.data.product;
 	$.ajax({
