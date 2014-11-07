020de7cbeb196d78174fa81ad689bd97e0b1fb13
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-08 07:59:48 +1100

    functionalise out service call

--- js/fcs_orig.js
+++ js/fcs_orig.js
@@ -1,21 +1,14 @@
-var hostAddress = '847055f0c7ed496490d0d5a13a805d55.cloudapp.net';
-$(document).ready(function () {
-	var varUrl; 
-	function CallService() {
-		$.ajax({
-			url : varUrl, // Location of the service
-			method: 'GET',
-			success : function(msg) { //On Successful service call
-						window.location = "activation.html";
-					  },
-			error: function(msg) { // When Service call fails
-						window.location = "buy.html";
-					  } 
-		});
-	}
-	
-	$("#btnEmailLicenceKey").click(function () {
-		varUrl = "http://"+ hostAddress + "/Service.svc/EmailLicence/?name=" + $('#name').val() + "&companyName=" + $('#company').val() + "&emailTo=" + $('#email').val() + "&productCode=SauceryNUnit";
-		CallService();
+function do_it(event) {
+	var hostAddress = '847055f0c7ed496490d0d5a13a805d55.cloudapp.net';
+	var url = "http://"+ hostAddress + "/Service.svc/EmailLicence/?name=" + $('#name').val() + "&companyName=" + $('#company').val() + "&emailTo=" + $('#email').val() + "&productCode=" + event.data.product;
+	$.ajax({
+		url : url,
+		method: 'GET',
+		success : function(msg) { //On Successful service call
+					window.location = "activation.html";
+				  },
+		error: function(msg) { // When Service call fails
+					window.location = "home.html";
+				  } 
 	});
-});
+}
