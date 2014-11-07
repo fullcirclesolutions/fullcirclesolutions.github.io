9e2128f3b3265b226fd2a24a153aa14467929a36
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-08 09:05:59 +1100

    remove alerts and unused code

--- js/fcs_orig.js
+++ js/fcs_orig.js
@@ -1,8 +1,4 @@
 function do_it(event) {
-	alert("name INSIDE=" + $('#name').val());
-	alert("company INSIDE=" + $('#company').val());
-	alert("email INSIDE=" + $('#email').val());
-	alert("product INSIDE=" + event.data.product);
 	var hostAddress = '847055f0c7ed496490d0d5a13a805d55.cloudapp.net';
 	var url = "http://"+ hostAddress + "/Service.svc/EmailLicence/?name=" + $('#name').val() + "&companyName=" + $('#company').val() + "&emailTo=" + $('#email').val() + "&productCode=" + event.data.product;
 	$.ajax({
@@ -13,6 +9,6 @@ function do_it(event) {
 				  },
 		error: function(msg) { // When Service call fails
 					window.location = "home.html";
-				  } 
+				  }
 	});
 }
