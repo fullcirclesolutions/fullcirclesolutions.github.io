51b77bad80f9a61528094936738f8ef0eb977e76
Andrew Gray <andrew.paul.gray@gmail.com>
2014-11-10 10:16:34 +1100

    test enable  of text fields

--- js/saucerynunit/thankyoucheck_orig.js
+++ js/saucerynunit/thankyoucheck_orig.js
@@ -2,3 +2,18 @@ var ref = document.referrer.toLowerCase();
 if (ref.indexOf("5ff4adab-3ef9-4788-b655-8c874cedfcb1.html") == -1) {
 	window.location.href = "home.html"
 }
+
+function doCheck(){
+        var allFilled = true;
+        $('input[type=text]').each(function(){
+            if($(this).val() == ''){
+                allFilled = false;
+                return false;
+            }
+        });
+        $('input[type=submit]').prop('disabled', !allFilled);
+}
+
+$(document).ready(function(){
+    $('input[type=text]').keyup(doCheck).focusout(doCheck);
+});
